539d6fa95e27c5336432493ec52e8260b759a330
Joe Charbonneau <j.charbonneau@ymail.com>
2020-10-12 21:21:12 -0400

    Added tracing to application

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+
+    // We are falling back to printing all spans at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    // Output the formatted spans to stdout
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    // The `with` method is provided by `SubscriberExt`, an extension
+    // trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // `set_global_default` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection = PgPool::connect(&configuration.database.conection_string())
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -15,6 +16,11 @@ pub async fn subscribe(
     payload: web::Form<SubscribeRequest>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!("Adding a new subscriber", %request_id, email = %payload.email, name = %payload.email);
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +32,10 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     .map_err(|e| {
-        eprintln!("Failed to execute query: {}", e);
+        tracing::error!("Failed to execute query: {:?}", e);
         HttpResponse::InternalServerError().finish()
     })?;
     Ok(HttpResponse::Ok().finish())
