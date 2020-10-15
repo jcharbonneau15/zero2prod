4cb1019219cf61e37fc8db39b151853e4f520838
Joe Charbonneau <j.charbonneau@ymail.com>
2020-10-14 21:53:53 -0400

    Added telemetry to test
    Setting TEST_LOG=true will show logs when running tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,6 +3,16 @@ use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+// Ensure that the "tracing" stack is only initialized once using lazy_static
+lazy_static::lazy_static! {
+    static ref TRACING: () = {
+        let filter = if std::env::var("TEST_LOG").is_ok() { "debug" } else { "" };
+        let subscriber = get_subscriber("test".into(), filter.into());
+        init_subscriber(subscriber);
+    };
+}
 
 pub struct TestApp {
     pub address: String,
@@ -10,6 +20,10 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    lazy_static::initialize(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
 
     // retreive the port assigned by the os
