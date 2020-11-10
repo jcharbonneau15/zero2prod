76c97e883b153f52594fd525499703c68726c194
Joe Charbonneau <j.charbonneau@ymail.com>
2020-11-09 23:04:02 -0500

    Timeout connection for postgres in 2 sec

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,7 +12,9 @@ async fn main() -> Result<(), anyhow::Error> {
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection = PgPool::connect(&configuration.database.conection_string())
+    let connection = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.conection_string())
         .await
         .map_err(anyhow::Error::from)
         .with_context(|| "Failed to connecto to Postgres")?;
